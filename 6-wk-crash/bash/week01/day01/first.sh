8bafe39860e0e6704dc64b9a9143013b74785428
Thuhantha Betti Pillippuge <thush_lak@yahoo.co.uk>
2026-08-17 22:08:25 +0100

    new

---
+++ 6-wk-crash/bash/week01/day01/first.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo " Bash Week 1 Day 1"
+echo " Hello Linux "
---
+++ 6-wk-crash/bash/week01/day01/variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name="Raspbery PI"
+kernel="Linux"
+Count="4"
+
+echo "$name"
+echo "$kernel"
+echo "$Count"
