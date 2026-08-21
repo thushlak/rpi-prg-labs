11a2e0faa20a1940a5ed04bda67680c28e5b382d
thushlak <49199116+thushlak@users.noreply.github.com>
2026-08-21 11:58:51 +0100

    Delete 6-wk-crash/bash/week01/day01 directory

--- 6-wk-crash/bash/week01/day01/first.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo " Bash Week 1 Day 1"
-echo " Hello Linux "
--- 6-wk-crash/bash/week01/day01/variables.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-name="Raspbery PI"
-kernel="Linux"
-Count="4"
-
-echo "$name"
-echo "$kernel"
-echo "$Count"
